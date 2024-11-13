3d504ec5ae8b5bc02a7788df7be780262e6209fa
Myron Zun Hao Weng <wengmyron@gmail.com>
2024-11-13 17:03:33 -0500

    commit files and folders
    add /codeForScanner folder, which contain /src and /bin folders, and /testPrograms folder, which contains .txt and .kay files for testing lexical analyzer. /src contains 3 .java files and /bin contains .class files of the .java files

---
+++ codeForScanner/src/ScannerDemo.java
@@ -0,0 +1,24 @@
+
+/**
+ * @author scharffc
+ * 
+ */
+public class ScannerDemo {
+
+	// This needs to be changed !
+	private static String file1 = "/Users/myron_zun_hao_weng/Desktop/Pace/Classes/2024 - 2025/Fall 2025/CS 361/HW5/GitHub/CS361Scanner/testPrograms/prog3.kay"; //string form of directory to file
+	private static int counter = 1;
+
+	public static void main(String args[]) {
+		TokenStream ts = new TokenStream(file1);
+		System.out.println(file1);
+
+		Token tk = new Token();
+		while (!ts.isEndofFile()) {
+			// TO BE COMPLETED
+
+			tk = ts.nextToken();
+			if (tk.getValue() != "") System.out.println("Token " + counter++ + " - Type: " + tk.getType() + " - Value: " + tk.getValue());
+		}
+	}
+}
