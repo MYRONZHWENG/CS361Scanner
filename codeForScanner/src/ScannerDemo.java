19be7b4c75365ea336c80eab1d38f43949b50217
Myron Zun Hao Weng <wengmyron@gmail.com>
2025-02-13 03:51:02 -0500

    test program edit

--- codeForScanner/src/ScannerDemo.java
+++ codeForScanner/src/ScannerDemo.java
@@ -6,7 +6,7 @@
 public class ScannerDemo {
 
 	// This needs to be changed !
-	private static String file1 = "/Users/myron_zun_hao_weng/Desktop/Pace/Classes/2024 - 2025/Fall 2025/CS 361/HW5(Scanner)/GitHub/CS361Scanner/testPrograms/program4.txt"; //string form of directory to file
+	private static String file1 = "/Users/myron_zun_hao_weng/Desktop/Pace/Classes/2024 - 2025/Fall 2024/CS 361/HW5(Scanner)/GitHub/CS361Scanner/testPrograms/program6.txt"; //string form of directory to file
 	private static int counter = 1;
 
 	public static void main(String args[]) {
